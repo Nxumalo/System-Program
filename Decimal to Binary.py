29152d638567bbb16fb357e13a554e299d4c9c7a
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-12-07 16:48:01 +0200

    Decimal to Binary

--- Decimal to Binary.py
+++ Decimal to Binary.py
@@ -1,7 +1,8 @@
 decimal_value=input("Enter a Decimal number:")
 value=int(decimal_value)
-binary=""
-while value>0:
+binary=" "
+
+while (value>0):
     binary=str(value%2)+binary
     value=value//2
 print("The binary number you've enter is",binary)
