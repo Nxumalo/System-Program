15f8505a711c23d974c75eeb166e763c8ea021d3
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-12-09 16:08:08 +0200

    Enum Switch Demo

--- EnumSwitchDemo.java
+++ EnumSwitchDemo.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class EnumSwitchDemo
 {
     enum Flavor {VANILLA, CHOCOLATE, STRAWBERRY};
-
+    
     public static void main(String[] args)
     {
         Flavor favorite = null;
