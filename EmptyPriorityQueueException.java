73a0b7189edb896a58325b75076b56f4c7e07817
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-12-09 16:07:16 +0200

    Empty Priority Queue Exception

--- EmptyPriorityQueueException.java
+++ EmptyPriorityQueueException.java
@@ -2,7 +2,7 @@ package net.datastructures;
 /**
  * Thrown when a priority queue cannot fulfill the requested operation
  * because it is empty.
- * @author Roberto Tamassia
+ * @author Fraser Nxumalo
  */
 public class EmptyPriorityQueueException  extends RuntimeException {
   public EmptyPriorityQueueException (String message) {
