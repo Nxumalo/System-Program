fa8ea2ec8b2f2ffcaaa003945ece86ec08f91c8e
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-12-09 16:35:11 +0200

    Huffman Tree

--- HuffmanTree.java
+++ HuffmanTree.java
@@ -1,7 +1,7 @@
 
 //<p><b>Author:<b>  Fraser Nxumalo.<p>
 //<p><b>Email:<b>   3538264@myuwc.ac.za<p>
-//<p>Last modified: 31/08/2020<p>
+//<p>Last modified: 09/12/2021<p>
 
 public class HuffmanTree{
 
